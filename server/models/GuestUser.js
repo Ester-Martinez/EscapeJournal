b18e0536ff57b0c995118b70187284ef57ca7aed
Ester-Martinez <martinez.ester@gmail.com>
2019-10-07 17:03:18 +0200

    little changes

--- server/models/GuestUser.js
+++ server/models/GuestUser.js
@@ -2,11 +2,8 @@ const mongoose = require('mongoose');
 const Schema   = mongoose.Schema;
 
 const userSchema = new Schema({
-  username: String,
-  password: String,
-  email: String,
-  userType: {type: String, enum: ['escapista', 'sala', 'admin'], default: 'escapista'},
   name: String,
+  email: String,
   roomsDone: [{ type : Schema.Types.ObjectId, ref: 'EscapeRooms' }]
 }, {
   timestamps: {
