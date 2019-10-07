dc9f4e375a9c387f2ecb03639f2ace199f7d1503
Ester-Martinez <martinez.ester@gmail.com>
2019-10-07 12:21:20 +0200

    first commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,77 +1,26 @@
-import React, { Component } from "react";
-import "./App.css";
-import { Switch, Route, Redirect } from "react-router-dom";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-import Navbar from "./components/navbar/Navbar";
-import Signup from "./components/auth/Signup";
-import Login from "./components/auth/Login";
-import AuthService from "./components/auth/AuthService";
-import Contents from "./components/contents/Contents";
-
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { loggedInUser: null };
-    this.service = new AuthService();
-
-    this.fetchUser()
-  }
-  getUser = userObj => {
-    this.setState({
-      loggedInUser: userObj,
-    });
-  };
-  fetchUser() {
-    return this.service
-      .loggedin()
-      .then(response => {
-        this.setState({
-          loggedInUser: response,
-        });
-      })
-      .catch(err => {
-        this.setState({
-          loggedInUser: false,
-        });
-      });
-  }
-  render ()  {
-//aqui hacemos rendering condicional dependiendo de si tenemos un usuario logeado o no
-if (this.state.loggedInUser) {
-  //en este caso mostramos los contenidos ya que hay usuario
+function App() {
   return (
-    <React.Fragment>
-      <Redirect to="/home" />
-
-      <div className="App">
-        <header className="App-header">
-          <Navbar userInSession={this.state.loggedInUser} logout={this.logout} />
-          {/* aqui simplemente se muestra un lorem ipsum genérico para que veáis contenidos que solo se muestran a usuarios logeados */}
-          <Contents />
-        </header>
-      </div>
-    </React.Fragment>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
-} else {
-  //si no estás logeado, mostrar opcionalmente o login o signup
-  return (
-    <React.Fragment>
-      <Redirect to="/login" />
-
-      <div className="App">
-        <header className="App-header">
-          <Navbar userInSession={this.state.loggedInUser} logout={this.logout} />
-          <Switch>
-            <Route exact path="/signup" render={() => <Signup getUser={this.getUser} />} />
-            <Route exact path="/login" render={() => <Login getUser={this.getUser} />} />
-          </Switch>
-        </header>
-      </div>
-    </React.Fragment>
-  );
-}
-  }
- 
 }
 
 export default App;
